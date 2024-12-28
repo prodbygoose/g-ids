15a6fed4565346b716ed611ed558ba885ae841ac
Goo$e <162994749+prodbygoose@users.noreply.github.com>
2024-12-28 13:10:13 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Goo$e'
+title 'g-ids'
+description 'Simple but functional id toggle script, Intended for Qbox'
+version '1.0.0'
+
+
+client_scripts {
+    'client.lua',
+}
