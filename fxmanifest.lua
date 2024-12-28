8b105503525473a78db5e3e0167c1268c841cbab
Goo$e <162994749+prodbygoose@users.noreply.github.com>
2024-12-28 14:04:51 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 author 'Goo$e'
 title 'g-ids'
-description 'Simple but functional id toggle script, Intended for Qbox'
+description 'Simple but functional ID toggle, Intended for Qbox'
 version '1.0.0'
 
 
